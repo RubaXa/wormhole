fe82dc651b94c5110e8fe713c9da63af4110937d
RubaXa <ibnRubaXa@gmail.com>
2014-09-11 19:23:38 +0400

    + uuid()

---
+++ src/uuid.js
@@ -0,0 +1,23 @@
+define([], function () {
+	var floor = Math.floor,
+		random = Math.random
+	;
+
+
+	function s4() {
+		return floor(random() * 0x10000 /* 65536 */).toString(16);
+	}
+
+
+	/**
+	 * UUID — http://ru.wikipedia.org/wiki/UUID
+	 * @returns {String}
+	 */
+	function uuid() {
+		return (s4() + s4() + "-" + s4() + "-" + s4() + "-" + s4() + "-" + s4() + s4() + s4());
+	}
+
+
+	// Export
+	return uuid;
+});
